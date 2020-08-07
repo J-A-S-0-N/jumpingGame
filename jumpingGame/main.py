e7e4d893c83f2e42dc8a72bd4b5f019ebd8078c4
J-4-S-0-N <62867830+J-A-S-0-N@users.noreply.github.com>
2020-08-08 00:32:50 +0800

    Add files via upload

---
+++ jumpingGame/main.py
@@ -0,0 +1,18 @@
+from os import system
+from time import sleep
+import random 
+
+def end(time):
+  while True:
+    print(time)
+    time -= 1
+    sleep(1)
+    if time == 0:
+      print("end")
+      sleep(2)
+      quit()
+def main():
+  gird1 = ["_"+"_"+"_"+"_"+"_"+"_"+"_"+"_"+"_"+"_"+"_"+"_"+"_"+"_"+"_"+"_"+"_"+"_"]
+
+if __name__ == "__main__":
+  main()
